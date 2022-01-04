42c8e67dafba891f9a69da72e282d50e0e34e434
ejcho3792 <ejcho3792@gmail.com>
2022-01-04 09:47:11 +0900

    [220104:total] first commit

---
+++ CodeUp/6078.py
@@ -0,0 +1,7 @@
+#[220104:total 02] first commit
+n='a'
+while n!='q':
+    n = input()
+    print(n)
+    if n=='q':
+        break
---
+++ CodeUp/6079.py
@@ -0,0 +1,8 @@
+#[220104:total 03] first commit
+n=int(input())
+nsum=0
+i=1
+while n>nsum:
+    nsum+=i
+    i+=1
+print(i-1)
